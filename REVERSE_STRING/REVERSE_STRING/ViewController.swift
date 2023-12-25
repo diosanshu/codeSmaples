b31bf6dd18f89f82d521cebff07ae66e001a7d6b
diosanshu <diosanshu@gmail.com>
2023-12-25 21:46:02 +0530

    Pair sum of array

--- REVERSE_STRING/REVERSE_STRING/ViewController.swift
+++ REVERSE_STRING/REVERSE_STRING/ViewController.swift
@@ -8,16 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
+        
+        
         super.viewDidLoad()
-        var reversStr = "12345"
-        var reversedStr = ""
-        for char in reversStr{
-        reversedStr =  String(char) + reversedStr
+        let orginalArray = [1,2,3,4,5,6,7,8,9]
+        var firstValue = 0
+        var lastvalue = orginalArray.count - 1
+        for _ in orginalArray{
+            let sumValues = orginalArray[firstValue] + orginalArray[lastvalue]
+            if sumValues == 10 {
+                print(orginalArray[firstValue] , orginalArray[lastvalue])
+                firstValue += 1
+                lastvalue -= 1
+            }
+            
         }
-        print(reversedStr)
-      //  output = 54321
+        
+//    output:
+//        1 9
+//        2 8
+//        3 7
+//        4 6
+//        5 5
+//        6 4
+//        7 3
+//        8 2
+//        9 1
+        
+        
     }
 }
 
