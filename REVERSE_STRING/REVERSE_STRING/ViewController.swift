bd8e7d94d676b3b1b027e88b0461ef310bd1a6ef
diosanshu <diosanshu@gmail.com>
2023-12-25 22:45:39 +0530

    No.of, occurrence

--- REVERSE_STRING/REVERSE_STRING/ViewController.swift
+++ REVERSE_STRING/REVERSE_STRING/ViewController.swift
@@ -11,13 +11,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        var reversStr = "12345"
-        var reversedStr = ""
-        for char in reversStr{
-        reversedStr =  String(char) + reversedStr
-        }
-        print(reversedStr)
-      //  output = 54321
+        let arr = ["FOO", "FOO", "BAR", "FOOBAR"]
+               var counts: [String: Int] = [:]
+
+               for item in arr {
+                   counts[item] = (counts[item] ?? 0) + 1
+               }
+
+               print(counts)  // "[BAR: 1, FOOBAR: 1, FOO: 2]"
+
+               for (key, value) in counts {
+                   print("\(key) occurs \(value) time(s)")
+               }
+
+      //  output
+//        ["FOOBAR": 1, "BAR": 1, "FOO": 2]
+        
+        
+//        FOOBAR occurs 1 time(s)
+//        BAR occurs 1 time(s)
+//        FOO occurs 2 time(s)
     }
 }
 
