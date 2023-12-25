17730956acbb92ba43b177f666ac7351dd9d4620
diosanshu <diosanshu@gmail.com>
2023-12-25 22:24:23 +0530

    Removing duplicates in string

--- REVERSE_STRING/REVERSE_STRING/ViewController.swift
+++ REVERSE_STRING/REVERSE_STRING/ViewController.swift
@@ -11,13 +11,37 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        var reversStr = "12345"
-        var reversedStr = ""
-        for char in reversStr{
-        reversedStr =  String(char) + reversedStr
-        }
-        print(reversedStr)
-      //  output = 54321
+        let str = "ANUANANU"
+                  var dublicateStr = ""
+                      for i in str{
+                          if  !dublicateStr.contains(i){
+                              dublicateStr.append(i)
+                          }
+                      }
+                  print(dublicateStr)
+        
+        //  output = ANU
+        
+        // second Way Remove sapces and duplicates
+
+        
+        let str1 = "1234 123       1235"
+              var dupStr = ""
+                    for val in str1 where  val != " "{
+                          if !dupStr.contains(val){
+                              dupStr.append(val)
+
+                         }
+                     }
+              print(dupStr)
+          }
+    //  output = 12345
+
+    
+    
+    
+  
+
     }
-}
+
 
