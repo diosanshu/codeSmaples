f0656559bb7d05fa0298fc253d61fbf0313ac6e3
diosanshu <diosanshu@gmail.com>
2023-12-25 23:58:42 +0530

    second max num

--- REVERSE_STRING/REVERSE_STRING/ViewController.swift
+++ REVERSE_STRING/REVERSE_STRING/ViewController.swift
@@ -8,16 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        var reversStr = "12345"
-        var reversedStr = ""
-        for char in reversStr{
-        reversedStr =  String(char) + reversedStr
+        let numbers = [3, 1,2]
+        if let secondMax = findSecondMax(numbers) {
+            print("The second maximum number is: \(secondMax)")
+        } else {
+            print("Array does not have enough elements.")
         }
-        print(reversedStr)
-      //  output = 54321
+        
+      //  output : The second maximum number is: 2
+
     }
+    func findSecondMax(_ array: [Int]) -> Int? {
+        guard array.count >= 2 else {
+            return nil // Array should have at least two elements
+        }
+
+        var max1 = Int.min
+        var max2 = Int.min
+
+        for num in array {
+            if num > max1 {
+                max2 = max1
+                max1 = num
+            } else if num > max2 && num < max1 {
+                max2 = num
+            }
+        }
+
+        return max2
+    }
+
 }
 
