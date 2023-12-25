327d21ab6f5450ef8e193fef08d80177a25e006f
diosanshu <diosanshu@gmail.com>
2023-12-25 22:56:14 +0530

    no.of occarance

--- REVERSE_STRING/REVERSE_STRING/ViewController.swift
+++ REVERSE_STRING/REVERSE_STRING/ViewController.swift
@@ -11,13 +11,25 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        var reversStr = "12345"
-        var reversedStr = ""
-        for char in reversStr{
-        reversedStr =  String(char) + reversedStr
-        }
-        print(reversedStr)
-      //  output = 54321
+        let mStr = "1212111"
+        let xchar : Character = "2"
+        let result = countAppearance(char: xchar, str: mStr)
+        print("Character \(xchar) occur \(result) times in '\(mStr)' string")
+
+
     }
+    func countAppearance(char: Character, str: String) -> Int {
+           var counter = 0
+           // Counting the occurrence of the
+           // specified character
+           for mChar in str {
+              if mChar == char {
+                 counter += 1
+              }
+           }
+           return counter
+        }
+
+  //  Character 2 occur 2 times in '1212111' string
 }
 
