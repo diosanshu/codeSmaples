3cb5555a68a8a9513eac9984e286a30f652c94d8
diosanshu <diosanshu@gmail.com>
2023-12-25 23:45:56 +0530

    Max n Min

--- REVERSE_STRING/REVERSE_STRING/ViewController.swift
+++ REVERSE_STRING/REVERSE_STRING/ViewController.swift
@@ -11,13 +11,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        var reversStr = "12345"
-        var reversedStr = ""
-        for char in reversStr{
-        reversedStr =  String(char) + reversedStr
-        }
-        print(reversedStr)
-      //  output = 54321
+    
+        MaxandMin(array: ["1","3","4","2"])
+      //  output = Max , Min  4 1
     }
+    func MaxandMin(array : [String]){
+               var max = array[0]
+               var min = array[0]
+              for i in array{
+                 if min > i {
+                     min = i
+                 }
+                 if max < i {
+                  max = i
+                 }
+              }
+        print("Max , Min ",max,min)
+
+          }
 }
 
