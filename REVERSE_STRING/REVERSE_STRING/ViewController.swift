e47616a9927926ca951785d1ad5e52098d71fb85
diosanshu <diosanshu@gmail.com>
2023-12-25 23:24:29 +0530

    constant - vowel

--- REVERSE_STRING/REVERSE_STRING/ViewController.swift
+++ REVERSE_STRING/REVERSE_STRING/ViewController.swift
@@ -11,13 +11,30 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        var reversStr = "12345"
-        var reversedStr = ""
-        for char in reversStr{
-        reversedStr =  String(char) + reversedStr
-        }
-        print(reversedStr)
-      //  output = 54321
+        print(getvowels(string: "Hello World"))
+
+      //  output = (constants: "Hll Wrld", vowels: "eoo")
     }
+    func getvowels(string:String) -> (constants:String, vowels:String) {
+           var Constants:String = ""//Stores constants
+           var Vowels:String = ""//Stores Vowels
+           for letter in string {//Iterate over string
+               switch letter.lowercased() {
+               case "a":
+                   Vowels.append(contentsOf: "\(letter)")
+               case "e":
+                   Vowels.append(contentsOf: "\(letter)")
+               case "i":
+                   Vowels.append(contentsOf: "\(letter)")
+               case "o":
+                   Vowels.append(contentsOf: "\(letter)")
+               case "u":
+                   Vowels.append(contentsOf: "\(letter)")
+               default://If no vowels are found add remaining characters to constants string
+                   Constants.append(contentsOf: "\(letter)")
+               }
+           }
+           return ("\(Constants)", "\(Vowels)")//Return tuple
+       }
 }
 
